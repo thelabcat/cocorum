9e9f2d45e59754b4177fa7906d0ca11483bb9351
Wilbur Jaywright <zargulthewizard@outlook.com>
2025-09-21 18:05:49 -0400

    Fixed TODO of Rumble timestamp parsing
    - Timestamp parser now respects timezone. HalleluJah!

--- src/cocorum/static.py
+++ src/cocorum/static.py
@@ -7,11 +7,17 @@ Copyright 2025 Wilbur Jaywright.
 
 This file is part of Cocorum.
 
-Cocorum is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Cocorum is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
 
-Cocorum is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+Cocorum is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License along with Cocorum. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License along
+with Cocorum. If not, see <https://www.gnu.org/licenses/>.
 
 S.D.G."""
 
@@ -164,8 +170,8 @@ class Misc:
     # Size of chat badge icons to retrieve, only valid one has long been the string 48
     badge_icon_size = "48"
 
-    # Rumble timestamp format, not including the 6 TODO characters at the end
-    timestamp_format = "%Y-%m-%dT%H:%M:%S"
+    # Rumble timestamp format
+    timestamp_format = "%Y-%m-%dT%H:%M:%S%z"
 
     # Key of the session token within the session cookie dict
     session_token_key = "u_s"
